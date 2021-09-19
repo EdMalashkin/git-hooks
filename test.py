a46e5c7b50a033de2cc7a8ddde735c420cc9d431
Guanglin Kuang <guanglin@alatest.com>
2021-09-19 13:40:09 +0200

    Create git hooks to standardize commit and code
    Three git hooks, namely "pre-commit", "commit-msg", "pre-push" are
    created to standardize the commit and code in this repo.
    
    pre-commit: This hook is run before every commit is created. It stops
    developers from creating commits in the master branch. Besides, it
    also checks if the Python code is formatted properly with black, which
    formats Python code according to PEP8.
    
    commit-msg: This hook checks the commit message entered by the author.
    If the exit status is 0, the commit message will be aborted. In this
    repo, the exit status of this hook is always 0, even if there are
    errors or warnings. The purpose is to keep what the author has entered
    and not discard it due to style issues. The author can modify the
    commit message according to the hints of this hook.
    
    pre-push: This hook is run before commits are pushed to the remote
    repository. It stops developers pushing to the master branch. Besides,
    it also does unit tests. If the unit tests fail, the push will be
    aborted.

---
+++ test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from app import division
+
+
+class TestDivision:
+    def test_division_normal(self):
+        assert division(55, 9) == (6, 1)
+
+    def test_division_divided_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            division(55, 0)
