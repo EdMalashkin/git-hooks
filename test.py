6707a5691ce413bbcc42a05b5795fa9b76f743b1
Edward Malashkin <emalashkin@gmail.com>
2023-06-20 15:47:15 +0200

    make it fail

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from app import division
 
 class TestDivision:
     def test_division_normal(self):
-        assert division(55, 9) == (6, 1)
+        assert division(55, 9) == (6, 2)
 
     def test_division_divided_by_zero(self):
         with pytest.raises(ZeroDivisionError):
